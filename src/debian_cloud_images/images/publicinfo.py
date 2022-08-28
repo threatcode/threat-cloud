b6d28c0b4fdb0e9a14b4ed8c327f50e694bede06
Ben Wilson <g0tmi1k@kali.org>
2022-08-28 16:40:56 +0100

    Drop repeated kali
    Was creating; kali-kali-rolling...
    
    Kali == Distro
    Kali-Rolling = Branch (aka release_id)
    Azure/EC2 = vendor
    AMD64/ARM64 = arch

--- src/debian_cloud_images/images/publicinfo.py
+++ src/debian_cloud_images/images/publicinfo.py
@@ -6,26 +6,26 @@ class ImagePublicType(enum.Enum):
     dev = {
         'azure_offer': 'kali-test',
         'azure_sku': '{release_id}',
-        'family': 'kali-{release_id}-{vendor}-{arch}-dev-{build_id}',
-        'path': '{release}/dev/{build_id}/kali-{release_id}-{vendor}-{arch}-dev-{build_id}-{version}',
-        'vendor_family': 'kali-{release_id}-{arch}-dev-{build_id}',
-        'vendor_description': 'Kali {release_id} (development build {build_id}-{version})',
+        'family': '{release_id}-{vendor}-{arch}-dev-{build_id}',
+        'path': '{release}/dev/{build_id}/{release_id}-{vendor}-{arch}-dev-{build_id}-{version}',
+        'vendor_family': '{release_id}-{arch}-dev-{build_id}',
+        'vendor_description': 'Kali Linux {release_id} (development build {build_id}-{version})',
     }
     daily = {
         'azure_offer': 'kali-{release_baseid}-daily',
         'azure_sku': '{release_id}',
-        'family': 'kali-{release_id}-{vendor}-{arch}-daily',
-        'path': '{release}/daily/{version}/kali-{release_id}-{vendor}-{arch}-daily-{version}',
-        'vendor_family': 'kali-{release_id}-{arch}-daily',
-        'vendor_description': 'Kali {release_id} (daily build {version})',
+        'family': '{release_id}-{vendor}-{arch}-daily',
+        'path': '{release}/daily/{version}/{release_id}-{vendor}-{arch}-daily-{version}',
+        'vendor_family': '{release_id}-{arch}-daily',
+        'vendor_description': 'Kali Linux {release_id} (daily build {version})',
     }
     release = {
         'azure_offer': 'kali-{release_baseid}',
         'azure_sku': '{release_id}',
-        'family': 'kali-{release_id}-{vendor}-{arch}',
-        'path': '{release}/{version}/kali-{release_id}-{vendor}-{arch}-{version}',
-        'vendor_family': 'kali-{release_id}-{arch}',
-        'vendor_description': 'Kali {release_id} ({version})',
+        'family': '{release_id}-{vendor}-{arch}',
+        'path': '{release}/{version}/{release_id}-{vendor}-{arch}-{version}',
+        'vendor_family': '{release_id}-{arch}',
+        'vendor_description': 'Kali Linux {release_id} ({version})',
     }
 
 
